12b709c1eb863f391dbb2b77980f16c6a3094452
lhafkamp <Luuk@Luuks-MacBook-Pro.local>
2017-04-10 17:22:32 +0200

    User input shows on screen

--- public/js/io.js
+++ public/js/io.js
@@ -1,2 +1,15 @@
 const socket = io();
-console.log('wuuut');
+const messageBox = document.querySelector('ul');
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const btn = document.querySelector('button');
+
+form.onsubmit = () => {
+	socket.emit('chat message', input.value);
+	input.value = '';
+	return false;
+};
+
+socket.on('chat message', (value) => {
+	messageBox.innerHTML += `<li>${value}</li>`;
+});
