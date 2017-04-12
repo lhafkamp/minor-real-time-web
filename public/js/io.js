47b564f090a52028271e26d3879ca5def0184faf
Luuk Hafkamp <luukhafkamp@gmail.com>
2017-04-12 22:31:00 +0200

    Feature/readme (#4)
    * added some comments first
    
    * long draft
    
    * update

--- public/js/io.js
+++ public/js/io.js
@@ -8,16 +8,19 @@ const loginInput = document.querySelector('#login input');
 const socket = io();
 let user = '';
 
+// receive the input value
 login.onsubmit = () => {
 	socket.emit('user', loginInput.value);
 	loginInput.value = '';
 	return false;
 }
 
+// store the input value
 socket.on('user', (userInput) => {
 	user = userInput;
 });
 
+// display the array that was stored in the server
 socket.on('thisArrayIsGoingPlaces', (userArray) => {
 	userBox.innerHTML = '';
 	userArray.forEach(newUser => {
@@ -25,6 +28,7 @@ socket.on('thisArrayIsGoingPlaces', (userArray) => {
 	});
 });
 
+// on disconnect, remove the user from the array and refresh the user list
 socket.on('delete', (userArray) => {
 	const removeUser = userArray.indexOf(user);
 	userArray.splice(removeUser, 1);
@@ -35,12 +39,14 @@ socket.on('delete', (userArray) => {
 	});
 });
 
+// receive the input value
 chat.onsubmit = () => {
 	socket.emit('chat message', chatInput.value);
 	chatInput.value = '';
 	return false;
 };
 
+// show the user + input value
 socket.on('chat message', (value) => {
 	messageBox.innerHTML += `<li><span>${user}:</span> ${value}</li>`;
 });
